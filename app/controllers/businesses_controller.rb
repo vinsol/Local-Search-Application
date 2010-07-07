a14bec3d5c833f2d2e6f9baa6a0b61699b0397d9
Jigar Patel <jagira@gmail.com>
2010-07-07 12:27:54 +0530

    minor changes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -77,14 +77,11 @@ class BusinessesController < ApplicationController
   end
 
   def send_to_phone
-    p "=====>>>>>>>><<<<<<<<<<========"
-    p params[:number]
     @business = Business.find_by_id(params[:id])
-    @business_details = "#{@business.name} - #{@business.contact_phone} - #{@business.contact_address}, #{business.location}, #{business.city}"
+    @business_details = "#{@business.name} - #{@business.contact_phone} - #{@business.contact_address}, #{@business.location}, #{@business.city}"
     
     @url_details = URI.escape(@business_details, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
     url = "http://s1.freesmsapi.com/messages/send?skey=11ae2fd4c2f0b7346d3cf11d97969778&message=#{@url_details}&recipient=#{params[:number]}"
-    p url
     Net::HTTP.get_print URI.parse(url)
     respond_to  do |format|
       format.js
--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,5 +1,5 @@
 class SubCategoriesController < ApplicationController
-
+  skip_before_filter :authorize, :only => [:index]
   def index
     @search_query = params[:search][SEARCH_REGEX,2]
     @sub_categories = SubCategory.find(:all, :conditions => ['sub_category LIKE ?', "%#{@search_query}%"])
